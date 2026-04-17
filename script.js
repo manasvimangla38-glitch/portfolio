86cf7863dac6d69b8ed17da3d6a85a1552641067
manasvimangla38-glitch <manasvimangla38@gmail.com>
2026-04-17 13:34:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function showMessage() {
+    alert("Thanks for visiting my portfolio!");
+}
